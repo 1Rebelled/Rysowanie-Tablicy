d4f16464e58dde4b72d6772d06c4116bc87d6f04
Rebelled <Rebelled@DESKTOP-O6CTR6P>
2023-02-24 12:39:24 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Deklaracja tablicy string ponieważ są znaki   6x6  dwuwymiarowa
+        string[,] TablicaZnakow = new string[6, 6] {
+            { "%", "%","%", "#","#", "#" },
+            { "%", "%","%", "#","#", "#"  },
+            { "%", "%","%", "#","#", "#"  },
+            { "*", "*","*", "+","+", "+"  },
+            { "*", "*","*", "+","+", "+"  },
+            { "*", "*","*", "+", "+", "+"  }
+        };
+        // Pętla w pętli
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Console.Write("{0}\t", TablicaZnakow[i, j]);
+            }
+            // przeniesienie do nastepnego wierszu
+            Console.WriteLine();
+        }
+
+    }
+
+}
